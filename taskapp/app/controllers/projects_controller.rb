df9a3bbd15201fd70796ae2bccabcbceee40ee9b
hiroyosi <chuno.hiroyoshi@gmail.com>
2014-12-07 04:07:25 +0900

    added form to add projects.

--- taskapp/app/controllers/projects_controller.rb
+++ taskapp/app/controllers/projects_controller.rb
@@ -6,4 +6,21 @@ class ProjectsController < ApplicationController
   def show
    @project = Project.find(params[:id])
   end
+
+  def new
+    @project = Project.new
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.save
+    redirect_to projects_path
+  end
+
+  private
+    # for filtering
+    def project_params
+      # titleだけを抽出する
+      params[:project].permit(:title)
+    end
 end
